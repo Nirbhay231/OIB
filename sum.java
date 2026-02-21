a5f6d48418e7137d15d25a0deaa78554f31a7427
Nirbhay231 <144772839+Nirbhay231@users.noreply.github.com>
2026-02-21 13:02:19 +0530

    Commit-1
    addition of two numbers

---
+++ sum.java
@@ -0,0 +1,12 @@
+public class sum{
+
+ public static int add(int a, int b)
+ {
+    int result=a+b;
+    return result;
+ }
+ public static void main(String args[])
+ {
+    System.out.println(add(3,2));
+ }
+}
