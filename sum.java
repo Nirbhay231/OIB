52ac2a8b2014c48af8fb7f54d33a78b68d54c992
Nirbhay231 <144772839+Nirbhay231@users.noreply.github.com>
2026-02-21 13:10:35 +0530

    Commit-2
    addition of 3 numbers

--- sum.java
+++ sum.java
@@ -1,12 +1,12 @@
 public class sum{
 
- public static int add(int a, int b)
+ public static int add(int a, int b, int c)
  {
-    int result=a+b;
+    int result=a+b+c;
     return result;
  }
  public static void main(String args[])
  {
-    System.out.println(add(3,2));
+    System.out.println(add(3,2,5));
  }
 }
